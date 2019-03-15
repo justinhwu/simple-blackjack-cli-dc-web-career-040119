ed1a3389e2ccf181df9eb18e3e69111815be897a
Justin Wu <justinhwu95@gmail.com>
2019-03-15 18:11:07 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -61,7 +61,7 @@ def runner
   # code runner here
   welcome
   currenthand = initial_round
-  until currenthand <21
+  until currenthand >21
     currenthand +=hit?(currenthand)
     display_card_total(currenthand)
 end
